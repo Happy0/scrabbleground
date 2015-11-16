ae3867e606cd20d14b67a1defa3eb5ed20cb596d
Gordon Martin <gordon.hugh.martin@gmail.com>
2015-11-16 21:20:10 +0000

    initial commit. begin immitating ornicar as usual.

---
+++ src/configure.js
@@ -0,0 +1,9 @@
+var merge = require('merge');
+
+module.exports = function(defaults, config) {
+
+    // If no configuration values were provided, return and use the default values
+    if(!config) return;
+
+    merge.recursive(data, config);
+}
---
+++ src/data.js
@@ -0,0 +1,53 @@
+require("./configure")
+
+var makeTile = function(letter, value) {
+    return {
+        "letter" : letter,
+        "value" : value
+    };
+}
+
+var makeSquare = function(tile, bonus) {
+    return {
+        "tile" : tile,
+        "bonus" : bonus
+    };
+}
+
+var squareBonuses =
+        [["TW","N","N","DL","N","N","N","TW","N","N","N","DL","N","N","TW"]
+       ,["N","DW","N","N","N","TL","N","N","N","TL","N","N","N","DW","N"]
+       ,["N","N","DW","N","N","N","DL","N","DL","N","N","N","DW","N","N"]
+       ,["DL","N","N","DW","N","N","N","DL","N","N","N","DW","N","N","DL"]
+       ,["N","N","N","N","DW","N","N","N","N","N","DW","N","N","N","N"]
+       ,["N","TL","N","N","N","TL","N","N","N","TL","N","N","N","TL","N"]
+       ,["N","N","DL","N","N","N","DL","N","DL","N","N","N","DL","N","N"]
+       ,["TW","N","N","DL","N","N","N","DW","N","N","N","DL","N","N","TW"]
+       ,["N","N","DL","N","N","N","DL","N","DL","N","N","N","DL","N","N"]
+       ,["N","TL","N","N","N","TL","N","N","N","TL","N","N","N","TL","N"]
+       ,["N","N","N","N","DW","N","N","N","N","N","DW","N","N","N","N"]
+       ,["DL","N","N","DW","N","N","N","DL","N","N","N","DW","N","N","DL"]
+       ,["N","N","DW","N","N","N","DL","N","DL","N","N","N","DW","N","N"]
+       ,["N","DW","N","N","N","TL","N","N","N","TL","N","N","N","DW","N"]
+       ,["TW","N","N","DL","N","N","N","TW","N","N","N","DL","N","N","TW"]];
+
+/**
+ *  Create an empty board
+ */
+var defaultSquares = squareBonuses.map(function (bonus) {
+    return makeSquare(null, bonus);
+})
+
+module.exports = function (cfg) {
+
+    var defaults = {
+        squares : defaultSquares
+    }
+
+    // Merge any specified configuration values with our defaults before returning
+    // the defaults
+    configure(defaults, cfg || {});
+
+    return defaults;
+}
+
