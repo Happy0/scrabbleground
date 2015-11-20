88ae08f4e509175ebde643ec854bafe532537cc9
Gordon Martin <gordon.hugh.martin@gmail.com>
2015-11-20 20:22:55 +0000

    y u no do anything?

--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
+var api = require('./api.js');
 var ctrl = require("./ctrl.js");
 var view = require ("./view.js");
 
+function init(element, config) {
+    var controller = new ctrl(config);
+
+    m.render(element, view(controller));
+
+    return api(controller);
+}
+
+module.exports = init;
 module.exports.controller = ctrl;
 module.exports.view = view;
+
+
