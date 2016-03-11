e9ff265ede347ea82c157ba8c643cc6eac5d207f
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-03-11 09:16:07 +0000

    Add helper function to get a stretch of tiles.

--- src/highlight.js
+++ src/highlight.js
@@ -7,8 +7,52 @@ module.exports = function(data, board) {
     bonusSquarePlaced: data.highlightedTileBonusSquareClass
   };
 
-  var highlightMove = function(placedMoveSummary) {
+  var getContiguousFiles = function(direction, startPosition) {
+    var currentX = startPosition.x;
+    var currentY = startPosition.y;
 
+    var incrementerInDirection = function () {
+      if (direction === "horizontal") {
+        currentX += 1;
+      } else {
+        currentY +=1;
+      }
+    }
+
+    var decrementerInDirection = function () {
+      if (direction === "horizontal") {
+        currentX -= 1;
+      }
+      else {
+        currentY -=1;
+      }
+    }
+
+    var tiles = [];
+
+    var walkAndPushTilesInDirection = function(incremeneterOrDecrementerFunction) {
+      // Reset and go in the other direction
+      currentX = startPosition.x;
+      currentY = startPosition.y;
+
+      do {
+        var currentTile = data.board[x][y].tile;
+        tiles.push(currentTile);
+
+        incremeneterOrDecrementerFunction();
+
+      } while (currentTile);
+    };
+
+    walkAndPushTilesInDirection(incrementerInDirection);
+    walkAndPushTilesInDirection(decrementerInDirection);
+
+    return tiles;
+  };
+
+  var highlightMove = function(placedMoveSummary) {
+    var placed = placedMoveSummary.placed;
+    var direction = placedMoveSummary.direction;
   };
 
   var removeAllHighlightedTiles = function() {
